35652c8434ecfc367eaf2fda071e6c2d3900e314
machariamwangi <you@example.com>
2019-08-30 08:43:37 -0400

    first commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,30 +1,32 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import {handleLeft} from '../Actions/Global'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleLeft } from "../Actions/Global";
 
-class Header extends Component{
-    constructor(props){
-        super(props)
-        this.handleBar = this.handleBar.bind(this);
-    }
-    handleBar(){
-        this.props.handleLeft(!this.props.leftbar)
-    }
-    render(){
-        return(
-            <header>
-                <div className="brand-w-button">
-                    <img className="brand" alt="logotype" src="/images/logo.png" />
-                    <i className="fa fa-bars" onClick={this.handleBar}></i>
-                </div>
-            </header>
-        )
-    }
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.handleBar = this.handleBar.bind(this);
+  }
+  handleBar() {
+    this.props.handleLeft(!this.props.leftbar);
+  }
+  render() {
+    return (
+      <header>
+        <div className="brand-w-button">
+          <i className="fa fa-bars" onClick={this.handleBar}></i>
+        </div>
+      </header>
+    );
+  }
 }
 
 function mapStateToProps(globalState) {
-    return {
-        leftbar: globalState.leftbar
-    };
+  return {
+    leftbar: globalState.leftbar
+  };
 }
-export default connect(mapStateToProps, {handleLeft})(Header);
+export default connect(
+  mapStateToProps,
+  { handleLeft }
+)(Header);
